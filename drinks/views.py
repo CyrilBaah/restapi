5a1842fb14841fe6259b243a21bb07034737b5d1
CyrilBaah <cyril.baah@amalitech.org>
2022-08-07 13:39:35 +0000

    get all drinks

--- drinks/views.py
+++ drinks/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Drink
+from .serializers import DrinkSerializer
+from django.http import JsonResponse
 
 # Create your views here.
+
+def drink_list(request):
+    drinks = Drink.objects.all()
+    serializer = DrinkSerializer(drinks, many=True)
+    return JsonResponse({ 'drinks': serializer.data })
+
